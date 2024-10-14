e3ea1534fe3f67b01dd0b68948a206765c66e901
PsychoLucidia <89222930+PsychoLucidia@users.noreply.github.com>
2024-10-14 21:24:00 +0800

    Update EnemyHurtbox.cs

--- Assets/Scripts/AI/EnemyHurtbox.cs
+++ Assets/Scripts/AI/EnemyHurtbox.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyHurtbox : MonoBehaviour
 {
     [SerializeField] Light orangeLight;
+    [SerializeField] SpiderStat spiderStat;
+    [SerializeField] float colorFloat;
+    [SerializeField] bool isEnemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,22 @@ public class EnemyHurtbox : MonoBehaviour
 
     public void LightIntensityTween(float intensity)
     {
+        if (isEnemy)
+        {
+            if (spiderStat.health >= (spiderStat.maxHealth * 0.60))   // color of health if it is below 100 but above 60 percent
+            {
+                orangeLight.color = Color.green;
+            }
+            else if (spiderStat.health >= (spiderStat.maxHealth * 0.25))  // color of health if it is below 60 but above 20 percent
+            {
+                orangeLight.color = Color.yellow;
+            }
+            else
+            {
+                orangeLight.color = Color.red; // color of health if it is below 20 percent
+            }
+        }
+
         orangeLight.intensity = intensity;
         LeanTween.value(orangeLight.intensity, 0, 0.3f).setOnUpdate((float val) => { orangeLight.intensity = val;}).setEaseOutCubic();
     }
